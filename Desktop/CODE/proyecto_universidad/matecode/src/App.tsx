b4d01446bcb62d65e0f73970aab984d8d86fde9c
Pablo Galindo <pablogalindo90@gmail.com>
2026-03-19 16:32:22 -0300

    First commit

---
+++ Desktop/CODE/proyecto_universidad/matecode/src/App.tsx
@@ -0,0 +1,15 @@
+import MateCode from './assets/MateCode.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <img src={MateCode} alt="Logo"/>
+      </section>
+    </>
+  )
+}
+
+export default App
