49bffb1b1524087bc420c3030cb8f4c5af132708
Pablo Galindo <pablogalindo90@gmail.com>
2026-05-04 08:34:59 -0300

    Resuelto un tema de configuracion de JSX

--- matecode/frontend/src/App.tsx
+++ matecode/frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import MateCode from './assets/MateCode.png'
 import './App.css'
 
---
+++ matecode/frontend/src/components/Landing.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Landing() {
+  return (
+    <>
+      <section id="center">
+        <h1>Welcome to MateCode!</h1>
+        <p>Your ultimate coding companion. Connect, collaborate, and code together in real-time. Join our community of developers and take your coding experience to the next level!</p>
+      </section>
+    </>
+  )
+}
