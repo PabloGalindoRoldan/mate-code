096affd4496c21cba309a174bf64c1dabb878176
Pablo Galindo <pablogalindo90@gmail.com>
2026-05-05 18:51:09 -0300

    Modelada la vista del login

--- matecode/frontend/src/App.tsx
+++ matecode/frontend/src/App.tsx
@@ -3,6 +3,7 @@ import './App.css'
 import Landing from './components/views/Landing';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import SysAdminView from './components/views/SysAdminView';
+import LoginView from './components/views/LoginView';
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/admin" element={<SysAdminView />} />
+        <Route path="/login" element={<LoginView />} />
       </Routes>
     </BrowserRouter>
   )
