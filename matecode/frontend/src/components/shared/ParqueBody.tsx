8f8496c9139b1c040824353faef0b504ded177e1
Pablo Galindo <pablogalindo90@gmail.com>
2026-05-08 21:50:30 -0300

    Agregado el mapa, funcionando al minimo, sin coreccion de bugs

--- matecode/frontend/src/components/shared/ParqueBody.tsx
+++ matecode/frontend/src/components/shared/ParqueBody.tsx
@@ -1,15 +1,16 @@
+import MapPanel from "./MapPanel";
 import "./ParqueBody.css"
 
 
 export default function ParqueBody(params: { empresaInfo: any, isMenuOpen: boolean }) {
     const { isMenuOpen } = params;
-    const { empresaInfo } = params;
+    //const { empresaInfo } = params;
+
 
     return (
         <>
             <div className={`ParqueBodyContainer ${isMenuOpen ? "shrunk" : "full"}`}>
-                <h1>Parque Industrial de Viedma</h1>
-                <p>{empresaInfo.descripcion}</p>
+                <MapPanel />
             </div>
         </>
     )
