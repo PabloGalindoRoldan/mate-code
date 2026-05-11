9f12f47872a9bfc30bbd4b3b709c56b3ba0fdb30
Pablo Galindo <pablogalindo90@gmail.com>
2026-05-11 11:21:09 -0300

    Agregado shuffler de paneles en empresa radicada

--- matecode/frontend/src/components/shared/EmpresaRadicadaBody.tsx
+++ matecode/frontend/src/components/shared/EmpresaRadicadaBody.tsx
@@ -1,16 +1,22 @@
+import { Car } from "lucide-react";
 import "./EmpresaRadicadaBody.css";
 import InfoPanel from "./InfoPanel";
 import StatisticsPanel from "./StatisticsPanel";
+import CargarConsumoPanel from "./CargarConsumoPanel";
 
 
-export default function EmpresaRadicadaBody(params: { empresaInfo: any, isMenuOpen: boolean }) {
+export default function EmpresaRadicadaBody(params: { empresaInfo: any, isMenuOpen: boolean, activeTab: string }) {
     const { isMenuOpen } = params;
     const { empresaInfo } = params;
+    const { activeTab } = params;
 
     return (
         <div className={`empresaRadicadaBody ${isMenuOpen ? "shrunk" : "full"}`}>
-            <InfoPanel empresaInfo={empresaInfo} />
-            <StatisticsPanel empresaInfo={empresaInfo} />
+            {activeTab === "info" && <InfoPanel empresaInfo={empresaInfo} />}
+            {activeTab === "info" && <StatisticsPanel empresaInfo={empresaInfo} />}
+            {activeTab === "consumos" && <CargarConsumoPanel />}
+            {activeTab === "messages" && <p>Mensajes - Esta sección está en construcción.</p>}
+            {activeTab === "settings" && <p>Configuración - Esta sección está en construcción.</p>}
         </div>
     );
 }
