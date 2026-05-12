0d0a65db1afb3ac57e60b32c9d63e0d957aab92e
Pablo Galindo <pablogalindo90@gmail.com>
2026-05-12 18:40:40 -0300

    merged

--- matecode/backend/parque-industrial/src/main/java/com/parque_industrial/entities/AdministradorParque.java
+++ matecode/backend/parque-industrial/src/main/java/com/parque_industrial/entities/AdministradorParque.java
@@ -5,20 +5,26 @@ import java.util.ArrayList;
 
 public class AdministradorParque extends Usuario {
 
-    public AdministradorParque(String nombre, String apellido, String email, String nombreUsuario, String contraseña) throws Exception {
-        super(nombre, apellido, email, nombreUsuario, contraseña);
-    }
-    //
-    //entiendo que el admin es quien decide si el proyecto se aprueba, rechaza o rectifica,
-    // si nosotros pusismos q el gestor de proyectos iba a contener todos los proyectos, entonces quien contenga un gestor
-    // de proyectos va a ser el admin, o el gestor va a tener administradores?
-   public void aprobarProyecto(Proyecto proyecto){
-        proyecto.aprobar();
-   }
-   public void rechazarProyecto(Proyecto proyecto){
-        proyecto.rechazar();
-   }
-   public void rectificarProyecto(Proyecto proyecto){
-        proyecto.rectificar();
-   }
+     public AdministradorParque(String nombre, String apellido, String email, String nombreUsuario, String contraseña)
+               throws Exception {
+          super(nombre, apellido, email, nombreUsuario, contraseña);
+     }
+
+     //
+     // entiendo que el admin es quien decide si el proyecto se aprueba, rechaza o
+     // rectifica,
+     // si nosotros pusismos q el gestor de proyectos iba a contener todos los
+     // proyectos, entonces quien contenga un gestor
+     // de proyectos va a ser el admin, o el gestor va a tener administradores?
+     public void aprobarProyecto(Proyecto proyecto) {
+          proyecto.aprobar();
+     }
+
+     public void rechazarProyecto(Proyecto proyecto) {
+          proyecto.rechazar();
+     }
+
+     public void rectificarProyecto(Proyecto proyecto) {
+          proyecto.rectificar();
+     }
 }
