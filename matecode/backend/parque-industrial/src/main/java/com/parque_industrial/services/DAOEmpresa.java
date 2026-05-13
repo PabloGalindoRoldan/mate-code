c49b3df28de8031ce2e44997e27a9f1e9f341649
Pablo Galindo <pablogalindo90@gmail.com>
2026-05-12 21:56:30 -0300

    "Efectuados Cambios en la configuracion del Backend para poder usar el endpoint de /registrar"

--- matecode/backend/parque-industrial/src/main/java/com/parque_industrial/services/DAOEmpresa.java
+++ matecode/backend/parque-industrial/src/main/java/com/parque_industrial/services/DAOEmpresa.java
@@ -1,12 +1,20 @@
-package com.parque_industrial.entities;
+package com.parque_industrial.services;
 
 import java.util.List;
+import com.parque_industrial.entities.Empresa;
+import com.parque_industrial.entities.Usuario;
 
-public interface GestorEmpresa {
-    public void cargarEmpresasRadicadas(List<Empresa> empresas); // este es el metodo para cargar las empresas ya existentes, cargar su respectivo lote
+public interface DAOEmpresa {
+    public void cargarEmpresasRadicadas(List<Empresa> empresas); // este es el metodo para cargar las empresas ya
+                                                                 // existentes, cargar su respectivo lote
+
     public void crearEmpresa(Empresa empresa);
+
     public List<Empresa> empresasRadicadas();
+
     public List<Empresa> empresasNoRedicadas();
+
     public List<Empresa> empresas();
+
     public void asignarRepresentante(Usuario representanteEmpresa);
 }
