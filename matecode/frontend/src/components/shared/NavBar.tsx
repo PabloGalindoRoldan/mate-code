d6105864c811b647e6f8c3571abedefa798601a7
Pablo Galindo <pablogalindo90@gmail.com>
2026-05-15 15:40:07 -0300

    Modificado el navbar para borrar los links a las vistas

--- matecode/frontend/src/components/shared/NavBar.tsx
+++ matecode/frontend/src/components/shared/NavBar.tsx
@@ -11,10 +11,6 @@ export default function NavBar() {
       <nav className="navbar">
         <h1 className="logo"><a onClick={() => navigate('/')}>RN</a></h1>
         <ul className="buttonList">
-          <li><button className="navButtonContacto" onClick={() => navigate('/parque')}>Parque</button></li>
-          <li><button className="navButtonContacto" onClick={() => navigate('/admin')}>Admin</button></li>
-          <li><button className="navButtonContacto" onClick={() => navigate('/nueva-empresa')}>Nueva Empresa</button></li>
-          <li><button className="navButtonContacto" onClick={() => navigate('/empresa-radicada')}>Empresa Radicada</button></li>
           <li><button className="navButtonContacto" onClick={() => navigate('/contacto')}>Contacto</button></li>
           <li><button className="navButtonIngresar" onClick={() => navigate('/login')}>Ingresar</button></li>
           <li><button className="navButtonRegistrarse" onClick={() => navigate('/register')}>Registrarse</button></li>
