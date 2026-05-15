3b4e814dda6c8165f84f47797b29395bbc8e7753
Pablo Galindo <pablogalindo90@gmail.com>
2026-05-15 19:11:52 -0300

    Terminada la integracion con el backend de opcion crear un nuevo administrador de parque

--- matecode/frontend/src/components/shared/SysAdminBody.tsx
+++ matecode/frontend/src/components/shared/SysAdminBody.tsx
@@ -3,24 +3,24 @@ import { useState } from 'react';
 import RegisterAdminView from '../views/RegisterAdminView';
 import { ChevronLeft } from "lucide-react";
 
-
 export default function SysAdminBody() {
-    const [registrar, setRegistrar] = useState(false)
+    const [registrar, setRegistrar] = useState(false);
 
     function handleClick() {
-        setRegistrar((registrar) => !registrar);
+        setRegistrar((prev) => !prev);
     }
-
 
     return (
         <div className="sysAdminBody">
-            {registrar ? <h2 className="adminTitle">Vista de administrador del Sistema</h2> : ""}
-            {registrar ? "" : <button className="botonAtras" onClick={handleClick}><ChevronLeft /></button>}
-            {registrar ?
-                <button onClick={handleClick} className="buttonRegistrarseLanding">Crear Nuevo Administrador de Parque</button> :
+            {!registrar ? <h2 className="adminTitle">Vista de administrador del Sistema</h2> : ""}
+            {registrar ? <button className="menu-toggle-btn" onClick={handleClick}><ChevronLeft /></button> : ""}
+            {!registrar ? (
+                <button onClick={handleClick} className="buttonRegistrarseLanding">
+                    Crear Nuevo Administrador de Parque
+                </button>
+            ) : (
                 <RegisterAdminView />
-            }
-
+            )}
         </div>
-    )
+    );
 }
