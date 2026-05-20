3ca3400860c5867dc24f75bed276f2b3bae5ec91
Pablo Galindo <pablogalindo90@gmail.com>
2026-05-20 10:01:27 -0300

    Cambios de estilos y arreglos esteticos

--- matecode/frontend/src/features/parque-dashboard/ParqueBody.tsx
+++ matecode/frontend/src/features/parque-dashboard/ParqueBody.tsx
@@ -3,6 +3,7 @@ import MapPanel from "./MapPanel";
 import PublicacionesPanel from "./PublicacionesPanel";
 import ReportesPanel from "./ReportesPanel";
 import "./ParqueBody.css";
+import LoadingSpinner from "../../ui/loading/LoadingSpinner";
 
 export default function ParqueBody(params: { empresaInfo: any, isMenuOpen: boolean, activeTab: string }) {
 
@@ -12,10 +13,12 @@ export default function ParqueBody(params: { empresaInfo: any, isMenuOpen: boole
         <div className={`ParqueBodyContainer ${isMenuOpen ? "shrunk" : "full"}`}>
             {activeTab === "map" && <MapPanel />}
             {activeTab === "messages" && <MensajeriaPanel />}
-            {activeTab === "companies" && <p>Empresas - Esta sección está en construcción.</p>}
+            {activeTab === "companies" && <LoadingSpinner text="Empresas - Esta sección está en construcción." />}
             {activeTab === "publications" && <PublicacionesPanel />}
             {activeTab === "reports" && <ReportesPanel />}
-            {activeTab === "settings" && <p>Configuración - Esta sección está en construcción.</p>}
+            {activeTab === "inventory" && <LoadingSpinner text="Gestión de Inventario - Esta sección está en construcción." />}
+            {activeTab === "budget" && <LoadingSpinner text="Gestión de Presupuesto - Esta sección está en construcción." />}
+            {activeTab === "settings" && <LoadingSpinner text="Configuración - Esta sección está en construcción." />}
         </div>
     );
 }
