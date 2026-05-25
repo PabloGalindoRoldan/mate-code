dfd772c71d4870e9e610190a017357bb8ee0691b
Aramis Sanchez Ayala <aramis.sanchezayala@gmail.com>
2026-05-25 20:50:34 -0300

    Dto, JDBC y mover los getters y setters a la clase abstracta

--- matecode/backend/parque-industrial/src/main/java/com/parque_industrial/entities/Proyecto.java
+++ matecode/backend/parque-industrial/src/main/java/com/parque_industrial/entities/Proyecto.java
@@ -161,4 +161,300 @@ public abstract class Proyecto {
     public void setEstado(String estado) {
         this.estado = estado;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUsuarioNombre() {
+        return usuarioNombre;
+    }
+
+    public void setUsuarioNombre(String usuarioNombre) {
+        this.usuarioNombre = usuarioNombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public void setActividadPrincipal(String actividadPrincipal) {
+        this.actividadPrincipal = actividadPrincipal;
+    }
+
+    public String getActividadSecundaria() {
+        return actividadSecundaria;
+    }
+
+    public void setActividadSecundaria(String actividadSecundaria) {
+        this.actividadSecundaria = actividadSecundaria;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(String rubro) {
+        this.rubro = rubro;
+    }
+
+    public String getDescripcionServicio() {
+        return descripcionServicio;
+    }
+
+    public void setDescripcionServicio(String descripcionServicio) {
+        this.descripcionServicio = descripcionServicio;
+    }
+
+    public String getPersonaReferente() {
+        return personaReferente;
+    }
+
+    public void setPersonaReferente(String personaReferente) {
+        this.personaReferente = personaReferente;
+    }
+
+    public String getMateriasPrimas() {
+        return materiasPrimas;
+    }
+
+    public void setMateriasPrimas(String materiasPrimas) {
+        this.materiasPrimas = materiasPrimas;
+    }
+
+    public String getDestinoProduccion() {
+        return destinoProduccion;
+    }
+
+    public void setDestinoProduccion(String destinoProduccion) {
+        this.destinoProduccion = destinoProduccion;
+    }
+
+    public void setSuperficieRequerida(Double superficieRequerida) {
+        this.superficieRequerida = superficieRequerida;
+    }
+
+    public Double getSuperficieTrabajo() {
+        return superficieTrabajo;
+    }
+
+    public void setSuperficieTrabajo(Double superficieTrabajo) {
+        this.superficieTrabajo = superficieTrabajo;
+    }
+
+    public Double getSuperficieDeposito() {
+        return superficieDeposito;
+    }
+
+    public void setSuperficieDeposito(Double superficieDeposito) {
+        this.superficieDeposito = superficieDeposito;
+    }
+
+    public Double getSuperficieCubierta() {
+        return superficieCubierta;
+    }
+
+    public void setSuperficieCubierta(Double superficieCubierta) {
+        this.superficieCubierta = superficieCubierta;
+    }
+
+    public Double getSuperficieEstacionamiento() {
+        return superficieEstacionamiento;
+    }
+
+    public void setSuperficieEstacionamiento(Double superficieEstacionamiento) {
+        this.superficieEstacionamiento = superficieEstacionamiento;
+    }
+
+    public String getTienePlanos() {
+        return tienePlanos;
+    }
+
+    public void setTienePlanos(String tienePlanos) {
+        this.tienePlanos = tienePlanos;
+    }
+
+    public String getLinkPlanos() {
+        return linkPlanos;
+    }
+
+    public void setLinkPlanos(String linkPlanos) {
+        this.linkPlanos = linkPlanos;
+    }
+
+    public void setEnergiaRequerida(Double energiaRequerida) {
+        this.energiaRequerida = energiaRequerida;
+    }
+
+    public void setPersonalAOcupar(Integer personalAOcupar) {
+        this.personalAOcupar = personalAOcupar;
+    }
+
+    public String getTensionAlimentacion() {
+        return tensionAlimentacion;
+    }
+
+    public void setTensionAlimentacion(String tensionAlimentacion) {
+        this.tensionAlimentacion = tensionAlimentacion;
+    }
+
+    public Double getPotenciaInstalada() {
+        return potenciaInstalada;
+    }
+
+    public void setPotenciaInstalada(Double potenciaInstalada) {
+        this.potenciaInstalada = potenciaInstalada;
+    }
+
+    public Double getAguaMensual() {
+        return aguaMensual;
+    }
+
+    public void setAguaMensual(Double aguaMensual) {
+        this.aguaMensual = aguaMensual;
+    }
+
+    public Double getGasMensual() {
+        return gasMensual;
+    }
+
+    public void setGasMensual(Double gasMensual) {
+        this.gasMensual = gasMensual;
+    }
+
+    public Double getResiduosCantidad() {
+        return residuosCantidad;
+    }
+
+    public void setResiduosCantidad(Double residuosCantidad) {
+        this.residuosCantidad = residuosCantidad;
+    }
+
+    public String getResiduosTipo() {
+        return residuosTipo;
+    }
+
+    public void setResiduosTipo(String residuosTipo) {
+        this.residuosTipo = residuosTipo;
+    }
+
+    public String getTratamientoEfluentes() {
+        return tratamientoEfluentes;
+    }
+
+    public void setTratamientoEfluentes(String tratamientoEfluentes) {
+        this.tratamientoEfluentes = tratamientoEfluentes;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTipoEmpresa() {
+        return tipoEmpresa;
+    }
+
+    public void setTipoEmpresa(String tipoEmpresa) {
+        this.tipoEmpresa = tipoEmpresa;
+    }
+
+    public String getPretensionTraslado() {
+        return pretensionTraslado;
+    }
+
+    public void setPretensionTraslado(String pretensionTraslado) {
+        this.pretensionTraslado = pretensionTraslado;
+    }
+
+    public String getEmplazamientoActual() {
+        return emplazamientoActual;
+    }
+
+    public void setEmplazamientoActual(String emplazamientoActual) {
+        this.emplazamientoActual = emplazamientoActual;
+    }
+
+    public String getTiempoRadicacion() {
+        return tiempoRadicacion;
+    }
+
+    public void setTiempoRadicacion(String tiempoRadicacion) {
+        this.tiempoRadicacion = tiempoRadicacion;
+    }
+
+    public String getBalanzaPublica() {
+        return balanzaPublica;
+    }
+
+    public void setBalanzaPublica(String balanzaPublica) {
+        this.balanzaPublica = balanzaPublica;
+    }
+
+    public String getComedor() {
+        return comedor;
+    }
+
+    public void setComedor(String comedor) {
+        this.comedor = comedor;
+    }
+
+    public String getSumCoworking() {
+        return sumCoworking;
+    }
+
+    public void setSumCoworking(String sumCoworking) {
+        this.sumCoworking = sumCoworking;
+    }
+
+    public String getCuitEmpresa() {
+        return cuitEmpresa;
+    }
+
+    public void setCuitEmpresa(String cuitEmpresa) {
+        this.cuitEmpresa = cuitEmpresa;
+    }
+
+    public LocalDateTime getFechaCreacion() {
+        return fechaCreacion;
+    }
+
+    public void setFechaCreacion(LocalDateTime fechaCreacion) {
+        this.fechaCreacion = fechaCreacion;
+    }
+
+    public LocalDateTime getFechaActualizacion() {
+        return fechaActualizacion;
+    }
+
+    public void setFechaActualizacion(LocalDateTime fechaActualizacion) {
+        this.fechaActualizacion = fechaActualizacion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- matecode/backend/parque-industrial/src/main/java/com/parque_industrial/persistence/proyecto/ProyectoDefinitivoJDBC.java
+++ matecode/backend/parque-industrial/src/main/java/com/parque_industrial/persistence/proyecto/ProyectoDefinitivoJDBC.java
@@ -1,12 +1,290 @@
 package com.parque_industrial.persistence.proyecto;
 
 import com.parque_industrial.dto.proyecto.CrearRequestDefinitivoDTO;
+import com.parque_industrial.entities.ProyectoDefinitivo;
+import com.parque_industrial.exceptions.DatabaseException;
 
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.sql.*;
 import java.util.List;
 
-public class ProyectoDefinitivoJDBC implements ProyectoDefinitivoDAO{
+@Repository
+public class ProyectoDefinitivoJDBC
+        implements ProyectoDefinitivoDAO {
+
+    private final DataSource dataSource;
+
+    public ProyectoDefinitivoJDBC(
+            DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Override
-    public List<CrearRequestDefinitivoDTO> listarDefinitivosPorCuit(String cuit) {
+    public List<CrearRequestDefinitivoDTO>
+    listarDefinitivosPorCuit(String cuit) {
         return List.of();
+    }
+
+    public void guardarProyectoDefinitivo(
+            ProyectoDefinitivo proyecto) {
+
+        String sql = """
+        INSERT INTO proyecto_definitivo (
+
+            usuario_nombre,
+            nombre,
+            descripcion,
+            actividad_principal,
+            actividad_secundaria,
+            telefono,
+            rubro,
+            descripcion_servicio,
+            persona_referente,
+            materias_primas,
+            destino_produccion,
+
+            superficie_requerida,
+            superficie_trabajo,
+            superficie_deposito,
+            superficie_cubierta,
+            superficie_estacionamiento,
+
+            tiene_planos,
+            link_planos,
+
+            energia_requerida,
+            personal_a_ocupar,
+            tension_alimentacion,
+            potencia_instalada,
+            agua_mensual,
+            gas_mensual,
+
+            residuos_tipo,
+            residuos_cantidad,
+            tratamiento_efluentes,
+            tipo_empresa,
+            direccion,
+            pretension_traslado,
+            emplazamiento_actual,
+            tiempo_radicacion,
+
+            balanza_publica,
+            comedor,
+            sum_coworking,
+
+            estado,
+            cuit_empresa,
+            fecha_creacion,
+            fecha_actualizacion,
+
+            link_viabilidad_financiera,
+            link_estudio_mercado,
+            link_impacto_ambiental,
+            link_habilitacion_municipal,
+            link_certificado_inhibiciones
+
+        )
+        VALUES (
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+        )
+        """;
+
+        try (Connection conn =
+                     dataSource.getConnection();
+
+             PreparedStatement ps =
+                     conn.prepareStatement(
+                             sql,
+                             Statement.RETURN_GENERATED_KEYS)) {
+
+            ps.setString(1,
+                    proyecto.getUsuarioNombre());
+
+            ps.setString(2,
+                    proyecto.getNombre());
+
+            ps.setString(3,
+                    proyecto.getDescripcion());
+
+            ps.setString(4,
+                    proyecto.getActividadPrincipal());
+
+            ps.setString(5,
+                    proyecto.getActividadSecundaria());
+
+            ps.setString(6,
+                    proyecto.getTelefono());
+
+            ps.setString(7,
+                    proyecto.getRubro());
+
+            ps.setString(8,
+                    proyecto.getDescripcionServicio());
+
+            ps.setString(9,
+                    proyecto.getPersonaReferente());
+
+            ps.setString(10,
+                    proyecto.getMateriasPrimas());
+
+            ps.setString(11,
+                    proyecto.getDestinoProduccion());
+
+            if (proyecto.getSuperficieRequerida() != null)
+                ps.setDouble(12,
+                        proyecto.getSuperficieRequerida());
+            else
+                ps.setNull(12, Types.DOUBLE);
+
+            if (proyecto.getSuperficieTrabajo() != null)
+                ps.setDouble(13,
+                        proyecto.getSuperficieTrabajo());
+            else
+                ps.setNull(13, Types.DOUBLE);
+
+            if (proyecto.getSuperficieDeposito() != null)
+                ps.setDouble(14,
+                        proyecto.getSuperficieDeposito());
+            else
+                ps.setNull(14, Types.DOUBLE);
+
+            if (proyecto.getSuperficieCubierta() != null)
+                ps.setDouble(15,
+                        proyecto.getSuperficieCubierta());
+            else
+                ps.setNull(15, Types.DOUBLE);
+
+            if (proyecto.getSuperficieEstacionamiento() != null)
+                ps.setDouble(16,
+                        proyecto.getSuperficieEstacionamiento());
+            else
+                ps.setNull(16, Types.DOUBLE);
+
+            ps.setString(17,
+                    proyecto.getTienePlanos());
+
+            ps.setString(18,
+                    proyecto.getLinkPlanos());
+
+            if (proyecto.getEnergiaRequerida() != null)
+                ps.setDouble(19,
+                        proyecto.getEnergiaRequerida());
+            else
+                ps.setNull(19, Types.DOUBLE);
+
+            if (proyecto.getPersonalAOcupar() != null)
+                ps.setInt(20,
+                        proyecto.getPersonalAOcupar());
+            else
+                ps.setNull(20, Types.INTEGER);
+
+            ps.setString(21,
+                    proyecto.getTensionAlimentacion());
+
+            if (proyecto.getPotenciaInstalada() != null)
+                ps.setDouble(22,
+                        proyecto.getPotenciaInstalada());
+            else
+                ps.setNull(22, Types.DOUBLE);
+
+            if (proyecto.getAguaMensual() != null)
+                ps.setDouble(23,
+                        proyecto.getAguaMensual());
+            else
+                ps.setNull(23, Types.DOUBLE);
+
+            if (proyecto.getGasMensual() != null)
+                ps.setDouble(24,
+                        proyecto.getGasMensual());
+            else
+                ps.setNull(24, Types.DOUBLE);
+
+            ps.setString(25,
+                    proyecto.getResiduosTipo());
+
+            if (proyecto.getResiduosCantidad() != null)
+                ps.setDouble(26,
+                        proyecto.getResiduosCantidad());
+            else
+                ps.setNull(26, Types.DOUBLE);
+
+            ps.setString(27,
+                    proyecto.getTratamientoEfluentes());
+
+            ps.setString(28,
+                    proyecto.getTipoEmpresa());
+
+            ps.setString(29,
+                    proyecto.getDireccion());
+
+            ps.setString(30,
+                    proyecto.getPretensionTraslado());
+
+            ps.setString(31,
+                    proyecto.getEmplazamientoActual());
+
+            ps.setString(32,
+                    proyecto.getTiempoRadicacion());
+
+            ps.setString(33,
+                    proyecto.getBalanzaPublica());
+
+            ps.setString(34,
+                    proyecto.getComedor());
+
+            ps.setString(35,
+                    proyecto.getSumCoworking());
+
+            ps.setString(36,
+                    proyecto.getEstado());
+
+            ps.setString(37,
+                    proyecto.getCuitEmpresa());
+
+            ps.setTimestamp(38,
+                    Timestamp.valueOf(
+                            proyecto.getFechaCreacion()));
+
+            ps.setTimestamp(39,
+                    Timestamp.valueOf(
+                            proyecto.getFechaActualizacion()));
+
+            ps.setString(40,
+                    proyecto.getLinkViabilidadFinanciera());
+
+            ps.setString(41,
+                    proyecto.getLinkEstudioMercado());
+
+            ps.setString(42,
+                    proyecto.getLinkImpactoAmbiental());
+
+            ps.setString(43,
+                    proyecto.getLinkHabilitacionMunicipal());
+
+            ps.setString(44,
+                    proyecto.getLinkCertificadoInhibiciones());
+
+            ps.executeUpdate();
+
+            try (ResultSet rs =
+                         ps.getGeneratedKeys()) {
+
+                if (rs.next()) {
+                    proyecto.setId(rs.getLong(1));
+                }
+            }
+
+        } catch (SQLException e) {
+
+            throw new DatabaseException(
+                    "Error al insertar proyecto definitivo",
+                    e
+            );
+        }
     }
 }
