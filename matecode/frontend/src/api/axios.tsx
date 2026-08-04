b020f09ffff8a188adf65da9f0a5282e90b5b990
Pablo Galindo <pablogalindo90@gmail.com>
2026-08-04 10:34:28 -0300

    cambiado el url para conectarse al back

--- matecode/frontend/src/api/axios.tsx
+++ matecode/frontend/src/api/axios.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'https://mate-code-production-b309.up.railway.app',
+    baseURL: 'https://mate-code-production.up.railway.app',
     headers: {
         'Content-Type': 'application/json',
     },
